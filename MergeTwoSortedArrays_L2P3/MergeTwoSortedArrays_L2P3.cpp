3e29a787a0c5d73605ede525055366b88c5a6507
Eric Pratt <ericprattusc@gmail.com>
2021-03-25 19:10:01 -0700

    added function outline

--- MergeTwoSortedArrays_L2P3/MergeTwoSortedArrays_L2P3.cpp
+++ MergeTwoSortedArrays_L2P3/MergeTwoSortedArrays_L2P3.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 
+int* merge(int arr_left[], int sz_left, int arr_right[], int sz_right) {
+    /* declaring an array using dynamic allocation of memory.
+        Merged the given two arrays into this third output Array */
+    int* arr_merged = new int[sz_left + sz_right];
+    // Add your code below this line. Do not modify any other code.
+
+
+
+
+    // Add your code above this line. Do not modify any other code.
+    /* retun the merged array */
+    return arr_merged;
+}
+
 
 int main()
 {
